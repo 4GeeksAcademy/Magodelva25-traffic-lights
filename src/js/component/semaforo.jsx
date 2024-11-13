bb55aabfb8a239899f8c45b18db739391e54c4fb
Mangodev <migudz@gmail.com>
2024-11-13 13:11:36 +0000

    basic version

---
+++ src/js/component/semaforo.jsx
@@ -0,0 +1,20 @@
+import React, { useState } from "react";
+
+
+	
+
+export const Semaforo = () => {
+    
+    const [color, setColor] = useState("red")
+    return (
+			<div className="wrapper">
+				<div className="section">
+                    <div className ="semaforo">
+                        <div onClick={() => setColor("red")} className={"light-red" + (color === "red" ? " brillo" : "" )}></div>
+                        <div onClick={() => setColor("yellow")} className={"light-yellow" + (color === "yellow" ? " brillo" : "" )}></div>
+                        <div onClick={() => setColor("green")} className={"light-green" + (color === "green" ? " brillo" : "" )}></div>
+                    </div>
+                </div>
+			</div>
+	);
+};
